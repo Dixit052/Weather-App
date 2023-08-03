aa35cfa5669dfc993c1236a08f19928df6cd9c70
Dixit052 <20bsm008@iiitdmj.ac.in>
2023-08-03 15:16:58 +0530

    Initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,6 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "c6a671265cfb5c2d0b0dc3f833d2e96b",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
